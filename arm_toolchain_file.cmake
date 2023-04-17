285c94a38bd3bb8f83efbbb82e1e3db33a2e9a25
jorge.guerra <j.a.guerra@protonmail.com>
2023-04-17 16:10:36 -0700

    added linker script

---
+++ arm_toolchain_file.cmake
@@ -0,0 +1,23 @@
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+set(CMAKE_SYSTEM_VERSION 1)
+
+#Toolchain binaries
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_ASM_COMPILER arm-none-eabi-g++)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(AS arm-none-eabi-as)
+set(AR arm-none-eabi-ar)
+set(OBJCOPY arm-none-eabi-objcopy)
+set(OBJDUMP arm-none-eabi-objdump)
+set(SIZE arm-none-eabi-size)
+
+set(MCU_FLAGS -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16)
+#Compiler flags
+set(COMMON_FLAGS -Wall -c --param max-inline-insns-single=50 -v -O3 -ffunction-sections -fdata-sections -g2 -Wall -Wextra -pedantic)
+
+set(CMAKE_C_FLAGS ${MCU_FLAGS} ${COMMON_FLAGS} -x c -std=gnu11)
+set(CMAKE_CXX_FLAGS ${MCU_FLAGS} ${COMMON_FLAGS} -fno-rtti -fno-exceptions -std=gnu++17 -fno-threadsafe-statics -nostdlib -Wno-register)
