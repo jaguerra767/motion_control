2e82a8c862a7303e64dc5dd3d4ed07dbdf13261b
jorge.guerra <j.a.guerra@protonmail.com>
2023-04-20 15:50:55 -0700

    added disconencted and setup visitor () overloads

---
+++ config.h
@@ -0,0 +1,14 @@
+//
+// Created by Jorge Guerra on 4/20/23.
+//
+
+#ifndef MOTION_CONTROL_CONFIG_H
+#define MOTION_CONTROL_CONFIG_H
+#include "ClearCore.h"
+#include <array>
+#include <cstdint>
+namespace config{
+    const IpAddress default_ip_addr = IpAddress(192,168,1,100);
+}
+
+#endif //MOTION_CONTROL_CONFIG_H
--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,7 @@
 #include "server.hpp"
 #include "ClearCore.h"
 #include "EthernetTcpServer.h"
+#include "../config.h"
 #include <cstdint>
 #include <optional>
 #include <variant>
@@ -12,11 +13,6 @@
 //https://www.cppstories.com/2018/06/variant/
 
 using namespace server;
-struct disconnected_t{};
-struct setup_t{};
-struct connected_t{};
-struct interrupted_t{};
-using state_t = std::variant<disconnected_t, setup_t, connected_t, interrupted_t>;
 namespace {
     constexpr int port_number = 8888;
     constexpr int max_packet_length = 100;
@@ -24,10 +20,62 @@ namespace {
     char out_packet_buffer[max_packet_length];
     bool use_dhcp = false; //TODO: This should be able to be configured via serial at commissioning (and on the fly?)
     std::uint32_t timeout = 5000; //TODO: This should be able to be configured via serial at commissioning (and on the fly?)
-    state_t connection_state = disconnected_t();
+    EthernetTcpClient clients[max_clients];
 }
 
+struct disconnected_t{};
 
+struct setup_t{};
+
+struct connected_t{
+    explicit connected_t(EthernetTcpServer&& server):_server(server){}
+private:
+    EthernetTcpServer _server;
+};
+
+struct interrupted_t{};
+
+using state_t = std::variant<disconnected_t, setup_t, connected_t, interrupted_t>;
+
+struct visit_connection {
+    state_t operator()(const disconnected_t&){
+        if(EthernetMgr.PhyLinkActive()){
+            return setup_t();
+        }
+    }
+    state_t operator()(const setup_t&){
+        EthernetMgr.Setup();
+        if(use_dhcp){
+            if(!EthernetMgr.DhcpBegin()){
+                return disconnected_t();
+            }
+        }else{
+            EthernetMgr.LocalIp(config::default_ip_addr);
+            auto server = EthernetTcpServer(port_number);
+            server.Begin();
+            return connected_t(std::move(server));
+        }
+        return disconnected_t();
+    }
+    state_t operator()(const connected_t&){}
+    state_t operator()(const interrupted_t&){}
+};
+
+
+state_t connection_state = disconnected_t();
+void connect(){
+    const auto* state = std::get_if<disconnected_t>(&connection_state);
+    if(state){
+        connection_state = setup_t();
+    }
+}
+
+void dhcp_enable(){
+    use_dhcp = true;
+}
+void dhcp_disable(){
+    use_dhcp = false;
+}
 
 
 
